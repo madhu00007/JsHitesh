5073e2a0a7d8d886dafaa4453fe40ddf1f0073d7
Mrinal M <46369323+madhu00007@users.noreply.github.com>
2024-07-01 10:07:09 +0000

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,21 @@
- 
+ "use strict";// treat all JS code as newer version
+
+ //alert(3 + 3) //We re using nodejs not browser
+
+ console.log(3
+    +
+    3)// code redabilty should be high
+
+    let name ="Mrinal";// string datatypes
+    let age = 18 // number datatypes & it ranges from 2 ^ 53
+    let isLoggedIn = true/false//Boolean datatypes
+
+    let x = 1234567890123456789012345n;//BigInt
+
+    //null = stand alone value (type = object)
+    //undefined = kindda place holder for a value which is not defined yet (type = undefined)
+    //symbol = uniquness
+
+    console.log(typeof "Mrinal");
+    console.log(typeof null);//output:object
+    console.log(typeof undefined);//output:undefined

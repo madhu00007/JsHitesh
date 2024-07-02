6e91a35501a5f9875cf92af1ea36d4baf5e37f05
Mrinal M <46369323+madhu00007@users.noreply.github.com>
2024-07-02 14:57:11 +0000

    Configuration done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+//let score =35;
+//let score ="35";
+//let score ="35abc";
+//let score =null;
+//let score =undefined;
+//let score =true;
+// let score =false;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+//console.log(valueInNumber);//output:NaN(not a number)
+//console.log(valueInNumber);//output:0
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true =>1; //false=>0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//"" =>false
+//"mrinal"=>true

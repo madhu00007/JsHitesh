c7312300e240d7a5c380dfee5a618452a2e1f0df
Mrinal M <46369323+madhu00007@users.noreply.github.com>
2024-07-03 05:19:54 +0000

    Config Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,9 +23,32 @@ let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1=>true; 0=>false
 //"" =>false
 //"mrinal"=>true
+
+//***********Operations************//
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+let str1 = "Hi!";
+let str2 = " Mrinal";
+let str3 = str1 + str2;
+//console.log(str3);
+
+//console.log("1" + 2); //output:12
+//console.log(1 + "2"); //output:12
+//console.log("1" + 2 + 2);//output:122
+//console.log(1 + 2 + "2");//output:32
+
+//console.log(+true);//output:1
+//console.log(true+);//output:error
+
+//console.log(+"");//output:0
+
+//postfix and prefix operator JS MDN ->read there
